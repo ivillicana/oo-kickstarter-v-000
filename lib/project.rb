74025db85dbe6ab4b94772a9890e25db47804e50
Isaac Villicana <villicana.i@gmail.com>
2018-04-18 18:32:43 +0000

    automatically backed up by learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,7 +1,12 @@
 class Project
-  attr_accessor :title
+  attr_accessor :title, :backer
 
   def initialization (title)
     @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    @backers << backer
   end
 end
